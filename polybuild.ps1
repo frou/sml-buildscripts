e913eff93407c2091748ef6146150d77f40f32e6
Chris Cannam <cannam@all-day-breakfast.com>
2021-03-18 17:46:13 +0000

    Assume same dir for the polyml binary as for its libraries; insist on console mode for things built this way; come up with a sensible output executable name

--- polybuild.ps1
+++ polybuild.ps1
@@ -8,18 +8,26 @@ Use Poly/ML to compile a Standard ML program defined in a .mlb file
 polybuild example.mlb
 Compile the Standard ML program defined in example.mlb using Poly/ML.
 
-The environment variable POLY_LIBDIR must be set to a directory
+The environment variable POLY_DIR must be set to a directory
 containing the Poly/ML library files (PolyLib.lib, PolyLib.dll,
-PolyMainLib.lib), and the clang compiler must be in the path, for
-example because we are running in a sufficiently recent Visual Studio
-command prompt with clang support.
+PolyMainLib.lib) and executable (PolyML.exe), and the clang compiler
+must be in the path, for example because we are running in a
+sufficiently recent Visual Studio command prompt with clang support.
 
 #>
 
-# NB to build the Poly/ML libraries, check out Poly/ML from git or
-# whatever, then go there in a VC command prompt and
+
+# NB just installing Poly/ML for Windows is not enough to get the
+# Poly/ML libraries, because the export libraries such as PolyLib.lib
+# are not included in the installation.
+
+# To build them, check out Poly/ML from git or whatever, then go to
+# its repo in a VC command prompt and
+
 # > msbuild polyml.sln /p:Configuration=Release '/t:PolyLib;PolyML;PolyMainLib'
-# then set the x64\Release subdir of the Poly/ML dir as POLY_LIBDIR
+
+# then set the x64\Release subdir of the Poly/ML dir as $env:POLY_DIR.
+
 
 Set-StrictMode -Version 2.0
 $ErrorActionPreference = "Stop"
@@ -36,12 +44,14 @@ if ($mlb -notmatch "[.]mlb$") {
   exit 1
 }
 
-$poly_libdir = $env:POLY_LIBDIR
+$poly_libdir = $env:POLY_DIR
 
 if (! $poly_libdir -or ! (Test-Path -PathType Container $poly_libdir)) {
-    "Error: POLY_LIBDIR environment variable must be set to a valid directory"
+    "Error: POLY_DIR environment variable must be set to a valid directory"
     exit 1
 }
+
+"Poly/ML libdir is $poly_libdir"
 
 $mydir = Split-Path -Parent $PSCommandPath
 
@@ -57,6 +67,9 @@ if ($lines -match "^Error: ") {
 $tmpfile_in = ([System.IO.Path]::GetTempFileName()) -replace "[.]tmp",".sml"
 $tmpfile_out = ([System.IO.Path]::GetTempFileName()) -replace "[.]tmp",".obj"
 
+$output_file = $mlb -replace ".mlb",""
+$output_file = "$output_file.exe"
+
 $outro = @"
 val _ = PolyML.export("$tmpfile_out", main);
 val _ = OS.Process.exit (OS.Process.success);
@@ -68,7 +81,9 @@ $script += $outro
 
 $script -replace "\\","\\" | Out-File -Encoding "ASCII" $tmpfile_in
 
-cat $tmpfile_in | polyml --error-exit | Out-Host
+$polyml = "$poly_libdir\polyml"
+
+cat $tmpfile_in | & $polyml -q --error-exit | Out-Host
 
 if (-not $?) {
     del $tmpfile_in
@@ -80,9 +95,7 @@ if (-not $?) {
 
 del $tmpfile_in
 
-"Poly/ML libdir is $poly_libdir"
-
-clang -O3 "$tmpfile_out" -o output.exe -Xlinker /subsystem:windows -Xlinker /ltcg -L"$poly_libdir" -lPolyLib -lPolyMainLib
+clang -O3 $tmpfile_out -o $output_file -Xlinker /subsystem:console -Xlinker /entry:WinMainCRTStartup -Xlinker /ltcg -L"$poly_libdir" -lPolyLib -lPolyMainLib
 
 if (-not $?) {
     del $tmpfile_out
